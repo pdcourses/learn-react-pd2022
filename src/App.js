f778bc2a5bb1a44126af82da40370c71aa1549a2
pdcourses <freshitpt@gmail.com>
2022-07-10 10:28:02 +0300

    basic jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { Component, Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+/*
+function App(){
+  return(
+    <p>Hello React!!!</p>
   );
 }
+*/
+
+let count = 100;
+
+class App extends Component{
+  render(){
+    return(
+      <>
+        <p>Hello React!!! {10+20} 1+1</p>
+        <p>text</p>
+        <p id={1}>item 1</p>
+        <p id={10-2}>item 1</p>
+        <p id={++count}>text</p>
+        <p id={++count}>text</p>
+        <p id={++count}>text</p>
+        <p id={++count}>text</p>
+      </>
+    )
+  }
+}
+
 
 export default App;
