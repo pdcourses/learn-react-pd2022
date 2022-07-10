d434f9485e1c6a18b7343a2d1f4007a23b175ff9
pdcourses <freshitpt@gmail.com>
2022-07-10 13:46:26 +0300

    userCard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import { Component, Fragment } from 'react';
 import './App.css';
-import Water from './Components/Water';
+import UserCard from './Components/UserCard';
+
+const currentUser1 = {
+  fullName: 'John Fox',
+  email: 'john@mail.com',
+  tel: '+5-123-12345-34',
+  imgSrc: 'https://img.freepik.com/premium-vector/man-developer-reports-about-new-startup-project_82574-8385.jpg?w=740'
+}
+const currentUser2 = {
+  fullName: 'Tom Tom',
+  email: 'john@mail.com',
+  tel: '+5-123-12345-34',
+  imgSrc: 'https://img.freepik.com/premium-vector/man-developer-reports-about-new-startup-project_82574-8385.jpg?w=740'
+}
+const currentUser3 = {
+  fullName: 'Ann Smith',
+  email: 'john@mail.com',
+  tel: '+5-123-12345-34',
+  imgSrc: 'https://img.freepik.com/premium-vector/man-developer-reports-about-new-startup-project_82574-8385.jpg?w=740'
+}
 
 
 export default class App extends Component{
@@ -10,9 +29,9 @@ export default class App extends Component{
   render(){
     return(
       <>
-        <Water temperature={-10}/>
-        <Water temperature={22}/>
-        <Water temperature={100}/>
+        <UserCard user={currentUser1} />
+        <UserCard user={currentUser2} />
+        <UserCard user={currentUser3} />
       </>
     )
   }
---
+++ src/Components/UserCard/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import styles from './UserCard.module.css';
+
+export default class UserCard extends Component {
+  constructor(props) {
+    super(props)  
+    this.state = {}
+  }
+  render() {
+    //const user = this.props.user;
+    const {fullName, email, tel, imgSrc} = this.props.user;
+    return (
+      <div className={styles.card_container}>
+        <img src={imgSrc} alt="photo"></img>
+        <h2>{fullName}</h2>   
+        <h4>{email}</h4>  
+        <h4>{tel}</h4>
+      </div>
+    )
+  }
+}
