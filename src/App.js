9b54d75bf0575e44b45fe43f17c7c51fc92c9519
pdcourses <freshitpt@gmail.com>
2022-07-10 11:59:15 +0300

    state for class component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,34 @@ import { Component, Fragment } from 'react';
 import './App.css';
 import Hello from './Components/Hello';
 import HelloClass from './Components/HelloClass';
-function App(){
-  return(   
-    <>
-      <Hello name={'John'} surname={'Smith'}/>
-      <Hello name={'Tom'} surname={'Fox'}/>
-      <Hello name={'Ann'} surname={'Tompson'}/>
-      <HelloClass name={"Vasya"} surname={"Ivanov"} />
-    </>
-  );
+
+const user = {
+  userName: 'Tom',
+  userSurname: 'Fox',
+  email: 'tom@gmail.com'
+};
+
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      userName: 'unknown',
+      userSurname: 'unknown'
+    };
+  }
+  loadUser = () => {
+    this.setState({
+      userName: user.userName,
+      userSurname: user.userSurname
+    });
+  }
+  render(){
+    return(
+      <>
+        <button onClick={this.loadUser}>load</button>
+        <HelloClass name={this.state.userName} surname={this.state.userSurname} />
+      </>
+    )
+  }
 }
-export default App;
+
