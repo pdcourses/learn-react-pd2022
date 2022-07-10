78310d6d96e60d2be058e39588185f9e55900a29
pdcourses <freshitpt@gmail.com>
2022-07-10 14:06:08 +0300

    delete state and use ternarka in hello component

--- src/Components/HelloClass/index.jsx
+++ src/Components/HelloClass/index.jsx
@@ -2,30 +2,14 @@ import React, { Component } from 'react'
 export default class HelloClass extends Component {
   constructor(props){
     super(props);
-    this.state = {
-      isHello: true,
-      msg: '',
-    }
-  }
-  changeMode = () => {
-    if(this.props.isHelloMode){
-      this.setState({
-        isHello: true,
-        msg: 'Hello'
-      });
-    } else{
-      this.setState({
-        isHello: false,
-        msg: 'Bye'
-      });
-    }
   }
   render() {
-    const {name, surname} = this.props;
-    this.changeMode();
+    const {name, surname, isHelloMode} = this.props;
     return (
         <>
-            <p>{this.state.msg}, {name} {surname}!</p>
+            <p>
+              {isHelloMode ? 'Hello' : 'Bye'}
+              , {name} {surname}!</p>
         </>
     )
   }
