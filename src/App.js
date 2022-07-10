7a217c0dab6704f0c140fda0578ab492123778e6
pdcourses <freshitpt@gmail.com>
2022-07-10 12:39:28 +0300

    water component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,18 @@
 import { Component, Fragment } from 'react';
 import './App.css';
-import Hello from './Components/Hello';
-import HelloClass from './Components/HelloClass';
+import Water from './Components/Water';
 
-const user = {
-  userName: 'Tom',
-  userSurname: 'Fox',
-  email: 'tom@gmail.com'
-};
 
 export default class App extends Component{
   constructor(props){
     super(props);
-    this.state = {
-      userName: 'unknown',
-      userSurname: 'unknown'
-    };
-  }
-  loadUser = () => {
-    this.setState({
-      userName: user.userName,
-      userSurname: user.userSurname
-    });
   }
   render(){
     return(
       <>
-        <button onClick={this.loadUser}>load</button>
-        <HelloClass name={this.state.userName} surname={this.state.userSurname} />
+        <Water temperature={-10}/>
+        <Water temperature={22}/>
+        <Water temperature={100}/>
       </>
     )
   }
---
+++ src/Components/Water/index.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import styles from './Water.module.css';
+
+export default class Water extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+        shape: 'water'
+    }
+    this.styleWater ='';
+  }
+  changeShape = () => {
+    const {temperature} = this.props;
+    let newShape;
+    if(temperature < 0){
+        newShape = 'ice';
+        this.styleWater = 'iceStyle';
+    } else if(temperature >= 100){
+        newShape = 'steam';
+        this.styleWater = 'steamStyle';
+    } else{
+        newShape = 'water';
+        this.styleWater = 'waterStyle';
+    }
+    this.setState({shape: newShape});
+
+  }
+  render() {
+    this.changeShape();
+    return (
+      <>
+        
+        <p className={styles[this.styleWater]}>{this.state.shape}</p>
+      </>
+    )
+  }
+}
