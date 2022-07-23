54bb403522cfc17e59a08cdfe9d3c299eda221e0
pdcourses <freshitpt@gmail.com>
2022-07-23 12:36:03 +0300

    timer hook

--- src/Components/ClockFun/index.jsx
+++ src/Components/ClockFun/index.jsx
@@ -2,28 +2,23 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types';
 
 export default function ClockFun(props) {
-    const [time1, setTime1] = useState(0);
-    const [time2, setTime2] = useState(0);
+    const [time, setTime] = useState(0);
     const [isRunning, setIsRunning] = useState(false);
     const reset = () => {
-        setTime1(0);
-        setTime2(0);
+        setTime(0);
     }
     useEffect (() => {
         if(isRunning){
-            const timerId1 = setInterval(() => {setTime1(time1 + 1)}, 1000);
-            const timerId2= setInterval(() => {setTime2(time2 - 1)}, 1000);
+            const timerId = setInterval(() => {setTime(time + props.step)}, 1000);
             return () => {
-                clearInterval(timerId1);
-                clearInterval(timerId2);
+                clearInterval(timerId);
             }
         }
 
-    }, [time1, time2, isRunning]);
+    }, [time, isRunning]);
     return (
         <>
-        <h1>Timer1 {time1}</h1>
-        <h1>Timer2 {time2}</h1>
+        <h1>Timer {time}</h1>
         <button onClick={() => setIsRunning(true)}>start</button>
         <button onClick={reset}>reset</button>
         <button onClick={() => setIsRunning(false)}>stop</button>
