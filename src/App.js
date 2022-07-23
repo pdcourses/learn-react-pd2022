df6beb754ae08aad5f938d3b29f512eaa71ab8c1
pdcourses <freshitpt@gmail.com>
2022-07-23 10:07:50 +0300

    simply form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React, { Component } from 'react'
-import Calendar from './Components/Calendar';
-import Card from './Components/Card';
+import SignInForm from './Components/SignInForm';
 
-const testUser = {
-  id: 1,
-  name: 'John',
-  surname: 'Smith'
-};
 export default class App extends Component {
   render() {
-    return (
-      <>
-         <Card user = {testUser} />
-      </>
-    )
+    return <SignInForm /> 
   }
 }
