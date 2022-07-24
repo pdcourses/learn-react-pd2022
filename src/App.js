674b2bd3fb72578e512ac036ac6d94047dc7a2c8
pdcourses <freshitpt@gmail.com>
2022-07-24 14:05:19 +0300

    clock matrix component:

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import Test from './Components/Test'
+import ClockMatrix from './Components/ClockMatrix'
 
 export default class App extends Component {
   render() {
-    return <Test />
+    return <ClockMatrix />
   }
 }
