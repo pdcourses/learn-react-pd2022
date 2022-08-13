2ff47313e8a03c3be36facef8a55e0730973a468
pdcourses <freshitpt@gmail.com>
2022-08-13 13:12:00 +0300

    outlet router component

--- src/App.js
+++ src/App.js
@@ -4,23 +4,16 @@ import About from './Pages/About';
 import Home from './Pages/Home';
 import Products from './Pages/Products';
 import NotFound from './Pages/NotFound';
+import MainMenu from './Components/MainMenu';
+import Header from './Components/Header';
 
-const Header = () => {
-  <header>
-    <h1>App page</h1>
-    <nav>
-      <NavLink to='/'>Home</NavLink>
-      <NavLink to='/about'>About</NavLink>
-      <NavLink to='/products'>Products</NavLink>
-    </nav>
-  </header>
-}
 
 export default function App() {
   return (
     <>
+    <Header />
       <Routes>
-          <Route path='/' element={<Header />}></Route>
+          <Route path='/' element={<MainMenu />} />
           <Route index element={<Home />} />
           <Route path='about' element={<About />} />
           <Route path='products/*' element={<Products />} />
