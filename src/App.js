b4727c23d2db2e9cd82ba6883e01b01e01823652
pdcourses <freshitpt@gmail.com>
2022-08-13 12:32:55 +0300

    routes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
-import Counter from './Components/Counter';
-import React, { Component } from 'react';
-import { StepContext } from './Contexts';
+import React from 'react'
+import {Link, Route, Routes, NavLink} from 'react-router-dom'
+import About from './Pages/About';
+import Home from './Pages/Home';
+import Products from './Pages/Products';
+import NotFound from './Pages/NotFound';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);  
-    this.state = {
-       step: 1,
-    }
-  }
-  handleStep = e => {
-    this.setState({step: Number(e.target.value)})
-  }
-  render() {
-    const {step} = this.state;
-    return (
-      <StepContext.Provider value={step}>
-          <Counter />
-      </StepContext.Provider>
-      /*
-      <div>
-        <input type="number" value={step} onChange={this.handleStep} />
-        <Counter step={step} />
-      </div>
-      */
-    )
-  }
+const Header = () => {
+  <header>
+    <h1>App page</h1>
+    <nav>
+      <NavLink to='/'>Home</NavLink>
+      <NavLink to='/about'>About</NavLink>
+      <NavLink to='/products'>Products</NavLink>
+    </nav>
+  </header>
+}
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+          <Route path='/' element={<Header />}></Route>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='products/*' element={<Products />} />
+          <Route path='*' element={<NotFound />} />
+      </Routes>
+    </>
+  )
 }
---
+++ src/Pages/Products/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Propducts() {
+  return (
+    <h1>Products page</h1>
+  )
+}
